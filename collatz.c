0cfc22e1fe83c1692710a7fed30fa05768da2104
BXM22 <brennen@csuf.fullerton.edu>
2024-09-28 10:26:15 -0700

    added_export

--- collatz.c
+++ collatz.c
@@ -3,38 +3,49 @@
 #include <time.h>
 
 
-int collatz_eq(int n)
+int collatz_eq(int n) //collatz equation
 {   
-    printf("%d ", n);
-    int term = n;
-    if (n == 1){
+    int steps = 0;
+    while (n != 1) {
+        if (n % 2 == 0) {
+            n = n / 2;
+        } else {
+            n = 3 * n + 1;
+        }
+        steps++;
+    }
+    return steps;
+}
+
+int test_collaz(int N, int MIN, int MAX) //generates file and puts the num and steps onto csv
+{
+    const char *filename = "collatz_output.csv"; 
+    FILE *fp = fopen(filename, "w");
+    if (fp == NULL) {
+        perror("Error opening file");
         return 1;
     }
-    else if (n % 2 == 0){
-        term = n/2;
-    }
-    else if (n % 2 == 1){
-        term = 3*n + 1;
-    }
-    return collatz_eq(term);
-}
-int test_collaz(int N, int MIN, int MAX){
-    int i;
-    for (i = 0; i < N; i++) {
+
+    fprintf(fp, "Number,Steps\n"); 
+
+    for (int i = 0; i < N; i++) {
         int randomNumber = (rand() % (MAX - MIN + 1)) + MIN;
-        printf("Collatz sequence for %d: \n", randomNumber);
-        int collatz = collatz_eq(randomNumber);
-        printf("\n");
+        fprintf(fp, "%d,", randomNumber); 
+
+        int steps = collatz_eq(randomNumber);
+        fprintf(fp, "%d\n", steps); 
     }
+
+    fclose(fp);
+    printf("Data exported to %s successfully.\n", filename);
     return 0;
 }
 
-
 int main(int argc, char *argv[])
 {
-    if (argc != 4){
+    if (argc != 4) {
         printf("Usage: %s <N> <MIN> <MAX>\n", argv[0]);
-       return 1;  
+        return 1;  
     }
 
     int N = atoi(argv[1]);
@@ -43,22 +54,10 @@ int main(int argc, char *argv[])
 
     srand(time(NULL)); 
 
-
-    test_collaz(N, MIN, MAX);
-
-    
-
-
+    if (test_collaz(N, MIN, MAX) != 0) {
+        printf("Error in processing.\n");
+        return 1;
+    }
 
     return 0;
-
-
-
-
-    
-    
-
-
-
-    
 }
