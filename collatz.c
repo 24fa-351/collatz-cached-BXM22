19581eeb1830af3b7248b0478e792ffc1e2cfcbd
BXM22 <brennen@csuf.fullerton.edu>
2024-09-29 11:38:26 -0700

    LRU_CACHE

--- collatz.c
+++ collatz.c
@@ -2,9 +2,7 @@
 #include <stdlib.h>
 #include <time.h>
 
-
 #define CACHE_CAPACITY 1000
-#define HASH_TABLE_SIZE 100000
 
 typedef struct Node {  //Node
     int key;
@@ -43,15 +41,94 @@ LRUCache* create_cache(int capacity) {
     return cache;
 }
 
+void move_to_front(LRUCache* cache, Node* node) {
+    if (cache->head == node) {
+        return; 
+    }
+    if (node->prev) {
+        node->prev->next = node->next;
+    }
+    if (node->next) {
+        node->next->prev = node->prev;
+    }
+    if (cache->tail == node) {
+        cache->tail = node->prev;
+    }
+    node->next = cache->head;
+    node->prev = NULL;
+    if (cache->head) {
+        cache->head->prev = node;
+    }
+    cache->head = node;
+
+    if (cache->tail == NULL) {
+        cache->tail = node;
+    }
+}
+
+void evict(LRUCache* cache) {
+    if (cache->tail == NULL) {
+        return; 
+    }
+    Node* toRemove = cache->tail;
+    cache->map[toRemove->key % CACHE_CAPACITY] = NULL;
+    if (cache->tail->prev) {
+        cache->tail->prev->next = NULL;
+    }
+    cache->tail = cache->tail->prev;
+    if (cache->tail == NULL) {
+        cache->head = NULL;
+    }
+    free(toRemove);
+    cache->size--;
+}
+
+void insert(LRUCache* cache, int key, int value) {
+    int index = key % CACHE_CAPACITY;
+
+    if (cache->map[index] != NULL && cache->map[index]->key == key) {
+        cache->map[index]->value = value;
+        move_to_front(cache, cache->map[index]);
+    } else {
+        
+        if (cache->size == cache->capacity) {
+            evict(cache); 
+        }
+        Node* newNode = create_node(key, value);
+        cache->map[index] = newNode;
+        newNode->next = cache->head;
+        if (cache->head) {
+            cache->head->prev = newNode;
+        }
+        cache->head = newNode;
+        if (cache->tail == NULL) {
+            cache->tail = newNode;
+        }
+        cache->size++;
+    }
+}
+
+int get(LRUCache* cache, int key) {
+    int index = key % CACHE_CAPACITY;
+
+    if (cache->map[index] != NULL && cache->map[index]->key == key) {
+        move_to_front(cache, cache->map[index]);
+        return cache->map[index]->value;
+    }
+    return -1; 
+}
 
 
 
-
-int collatz_eq(int n) //collatz equation
+int collatz_eq(int n, LRUCache* cache) //collatz equation
 {   
 
-    int cacheResult;
+    int cacheResult = get(cache,n);
+    if (cacheResult != -1) {
+        return cacheResult;
+    }
     int steps = 0;
+    int num = n;
     while (n != 1) {
         if (n % 2 == 0) {
             n = n / 2;
@@ -60,11 +137,13 @@ int collatz_eq(int n) //collatz equation
         }
         steps++;
     }
+    insert(cache,num,steps);
     return steps;
 }
 
 int test_collaz(int N, int MIN, int MAX) //generates file and puts the num and steps onto csv
 {
+    LRUCache* cache = create_cache(CACHE_CAPACITY);
     const char *filename = "collatz_output.csv"; 
     FILE *fp = fopen(filename, "w");
     if (fp == NULL) {
@@ -78,12 +157,12 @@ int test_collaz(int N, int MIN, int MAX) //generates file and puts the num and s
         int randomNumber = (rand() % (MAX - MIN + 1)) + MIN;
         fprintf(fp, "%d,", randomNumber); 
 
-        int steps = collatz_eq(randomNumber);
+        int steps = collatz_eq(randomNumber,cache);
         fprintf(fp, "%d\n", steps); 
     }
 
     fclose(fp);
-    printf("Data exported to %s successfully.\n", filename);
+    printf("Steps exported to %s.\n", filename);
     return 0;
 }
 
